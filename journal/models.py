7f2fd4437845ab835524923295a4a00068ceae28
AYAdesuka <sashapanchehin2007@gmail.com>
2026-05-14 21:59:30 +0300

    make forms registration and login

--- journal/models.py
+++ journal/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -10,7 +11,7 @@ class Person(models.Model):
         ('админ', 'Админ'),
     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='person')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='person')
     role = models.CharField(max_length=20, choices=PERSON_STATUS, default="student")
     last_name = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
