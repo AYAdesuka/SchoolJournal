0d6cc18d4a85f2d4dff0c68c04861a916582ae3b
AYAdesuka <sashapanchehin2007@gmail.com>
2026-05-20 04:09:14 +0300

    create registration form

--- SchoolJournal/urls.py
+++ SchoolJournal/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from main import views as main_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_view.home, name='home'),
-
+    path('login', main_view.login, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('registration/', main_view.registration, name='registration'),
 ]
