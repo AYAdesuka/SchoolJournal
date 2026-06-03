9e88e114d48c50bf12812961c23f9b40e5933b6a
AYAdesuka <sashapanchehin2007@gmail.com>
2026-06-03 06:45:06 +0300

    create custom matrix

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from accounts.models import CustomUser
-from journal.models import SchoolClass, Subject, GradeBook
+from journal.models import SchoolClass, Enrollment, GradeBook, Grade
+from django.http import JsonResponse
 
 
 def home(request):
@@ -28,96 +29,80 @@ def class_gradebook(request, class_id):
         class_teacher=request.user
     )
 
-    return render(request, 'class_gradebook.html', {'school_class': school_class})
+    grade_map = {}
 
-@login_required
-def class_subjects(request, class_id):
-    school_class = get_object_or_404(
-        SchoolClass,
-        id=class_id,
-        class_teacher=request.user
-    )
-    print(Subject.objects.all())
-    gradebooks = GradeBook.objects.filter(school_class=school_class).select_related('subject')
+    for g in Grade.objects.filter(gradebook__school_class=school_class):
+        grade_map[(g.student_id, g.gradebook_id)] = g.grade_value
 
-    return render(
-        request,
-        'sections/subjects.html',{'school_class': school_class,'gradebooks': gradebooks})
+    GRADE_CHOICES = [1, 2, 3, 4, 5]
 
-@login_required
-def class_teachers(request, class_id):
-    school_class = get_object_or_404(
-        SchoolClass,
-        id=class_id,
-        class_teacher=request.user
+    enrollments = (
+        Enrollment.objects
+        .filter(school_class=school_class)
+        .select_related('student')
+        .order_by('student__last_name', 'student__first_name')
     )
 
-    return render(request, 'sections/teachers.html', {'school_class': school_class})
-
-@login_required
-def class_grades(request, class_id):
-    school_class = get_object_or_404(
-        SchoolClass,
-        id=class_id,
-        class_teacher=request.user
+    lessons = (
+        GradeBook.objects
+        .filter(school_class=school_class)
+        .select_related('subject')
+        .order_by('lesson_date', 'lesson_number')
     )
 
-    return render(request, 'sections/grades.html', {'school_class': school_class})
+    student_rows = []
+
+    for enrollment in enrollments:
+        student = enrollment.student
+
+        row = {
+            'student': student,
+            'grades': []
+        }
+
+        for lesson in lessons:
+            grade = Grade.objects.filter(
+                student=student,
+                gradebook=lesson
+            ).first()
+
+            row['grades'].append(
+                grade.grade_value if grade else ''
+            )
+
+        student_rows.append(row)
+
+    return render(request, 'class_gradebook.html', {
+        'school_class': school_class,
+        'enrollments': enrollments,
+        'lessons': lessons,
+        'grade_map': grade_map,
+        'grades': [1, 2, 3, 4, 5],
+    })
+
 
 @login_required
-def class_attendance(request, class_id):
-    school_class = get_object_or_404(
-        SchoolClass,
-        id=class_id,
-        class_teacher=request.user
-    )
+def update_grade(request):
+    if request.method == "POST":
 
-    return render(request, 'sections/attendance.html', {'school_class': school_class})
+        student_id = request.POST.get("student_id")
+        gradebook_id = request.POST.get("gradebook_id")
+        grade_value = request.POST.get("grade_value")
 
-@login_required
-def class_stats(request, class_id):
-    school_class = get_object_or_404(
-        SchoolClass,
-        id=class_id,
-        class_teacher=request.user
-    )
+        gradebook = get_object_or_404(GradeBook, id=gradebook_id)
+        student = get_object_or_404(CustomUser, id=student_id)
 
-    return render(request, 'sections/stats.html', {'school_class': school_class})
+        if not grade_value:
+            Grade.objects.filter(
+                student=student,
+                gradebook=gradebook
+            ).delete()
+            return redirect(request.META.get("HTTP_REFERER"))
 
-@login_required
-def class_homework(request, class_id):
-    school_class = get_object_or_404(
-        SchoolClass,
-        id=class_id,
-        class_teacher=request.user
-    )
+        Grade.objects.update_or_create(
+            student=student,
+            gradebook=gradebook,
+            defaults={"grade_value": grade_value}
+        )
 
-    return render(request, 'sections/homework.html', {'school_class': school_class})
-
-@login_required
-def class_docs(request, class_id):
-    school_class = get_object_or_404(
-        SchoolClass,
-        id=class_id,
-        class_teacher=request.user
-    )
-
-    return render(request, 'sections/docs.html', {'school_class': school_class})
-
-@login_required
-def class_settings(request, class_id):
-    school_class = get_object_or_404(
-        SchoolClass,
-        id=class_id,
-        class_teacher=request.user
-    )
-
-    return render(request, 'sections/settings.html', {'school_class': school_class})
-
-@login_required
-def subject_grades(request, class_id, subject_id):
-
-    school_class = get_object_or_404(SchoolClass,id=class_id, class_teacher=request.user)
-    subject = get_object_or_404(Subject,id=subject_id)
-
-    return render(request,'sections/subject_grades.html',{'school_class': school_class,'subject': subject,})
+        return redirect(request.META.get("HTTP_REFERER"))
